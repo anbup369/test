d458832ae1514b98d33006d7f9097ad6f96bb27c
anbu369 <anbu.vpm@gmail.com>
2025-06-22 16:51:42 +0530

    test java file

---
+++ class.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Classify_triangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input the lengths of the three sides
+        System.out.print("Enter side A: ");
+        double a = scanner.nextDouble();
+        System.out.println("side A is: "+a);
+
+
+
+    }
+}
