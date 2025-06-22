52c8c64d710eea7189e8e434453ce45ca132145b
anbup369 <anbu.vpm@gmail.com>
2025-06-22 16:53:31 +0530

    Update class.java
    removed sample program and made it as program structure

--- class.java
+++ class.java
@@ -1,14 +1,10 @@
 
-import java.util.Scanner;
 
-public class Classify_triangle {
+
+public class Class_Name {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        // Input the lengths of the three sides
-        System.out.print("Enter side A: ");
-        double a = scanner.nextDouble();
-        System.out.println("side A is: "+a);
+        System.out.println("Class_Name and main function structure);
 
 
 
